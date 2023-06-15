88f51ceb74d87fd9c3f909904b1bc900a7d67fe2
Goodness <goodnessabisoye@gmail.com>
2023-06-15 01:19:34 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 
 int main(){
 
-	puts( "Programming is like building a multilingual puzzle, followed by a new line" );
+	puts( "\"Programming is like building a multilingual puzzle\n" );
 
 	return 0;
 }
