17a5b74e045578bf8b31db20d2ec0ac8b4f78f1a
Goodness <goodnessabisoye@gmail.com>
2023-06-15 01:16:49 -0700

    first c statement with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/**
+ * Write a C program that prints exactly "Programming is 
+ * like building a multilingual puzzle, followed by a new 
+ * line"
+ * Use the function puts
+ * You are not allowed to use printf
+ * Your program should end with the value 0
+ */
+
+#include <stdio.h>
+
+int main(){
+
+	puts( "Programming is like building a multilingual puzzle, followed by a new line" );
+
+	return 0;
+}
