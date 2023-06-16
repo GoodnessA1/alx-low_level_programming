8a96fc0e8f80b5756cd770b8c5e3dce4e0517144
Goodness <goodnessabisoye@gmail.com>
2023-06-16 07:08:20 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ * @i: int
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+
+	putchar('\n');
+	return (0);
+}
