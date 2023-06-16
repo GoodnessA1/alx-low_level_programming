2cc19fcb4bd40bf4571dc1669389f0f5cc0113d9
Goodness <goodnessabisoye@gmail.com>
2023-06-16 07:43:46 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print number in string format
+ * @i:int
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
