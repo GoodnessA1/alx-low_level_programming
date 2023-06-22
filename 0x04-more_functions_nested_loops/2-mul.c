fda64fb33a99647fd69e50417650f15a437b6c29
Goodness <goodnessabisoye@gmail.com>
2023-06-22 11:16:24 -0700

    C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int mul(int a, int b);
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ * @result: stores the product
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
