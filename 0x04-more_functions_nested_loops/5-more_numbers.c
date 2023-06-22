153faf5c69727c0d81daf7abda64af444c109b8b
Goodness <goodnessabisoye@gmail.com>
2023-06-22 11:44:19 -0700

    C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,13 @@
 void more_numbers(void)
 {
 
-	int a = 0;
-	int b = 0;
+	int a;
+	int b;
 
-	while (b <= 9)
+	for (b = 0; b < 11; b++)
 	{
-		while (a <= 14)
-		{
-			if (a > 9)
-				_putchar(a / 10 + '0');
-			_putchar(a % 10 + '0');
-			a++;
-
-		}
-	_putchar('\n');
-	b++;
-	a = 0;
+		for (a = 0; a <= 14; a++)
+			_putchar(a + '0');
+		_putchar('\n');
+	}	
 }
