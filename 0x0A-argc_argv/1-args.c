5ec12346ffd3b69a0a592116ca2f0e03b2767bd9
Goodness <goodnessabisoye@gmail.com>
2023-07-06 23:36:07 -0700

    C

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.c>
+#include "main.h"
+
+/**
+ * main - Prints number of arguments
+ * @argc: argument count
+ * @argv: argument variables
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+
+	return (0);
+}
