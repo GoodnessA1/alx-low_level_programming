0f7c2243aa979009ae9a65c5b4f38f7b786262d5
Goodness <goodnessabisoye@gmail.com>
2023-07-06 23:31:12 -0700

    Command line arguments in c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the name of the file
+ * @argc: argument count
+ * @argv: argument variable
+ *
+ * Return: 0(successful)
+ */
+int main(int argc, char **argv)
+{
+	puts(argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+# ARGC and ARGV in c
+
+## USAGE:
+
+`./filename arg1 arg2 ... argn`
