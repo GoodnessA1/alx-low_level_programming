8b1af678d87acbc18cb05a3149a62383b4bb90bf
Goodness <goodnessabisoye@gmail.com>
2023-07-17 07:19:50 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * dog - Its a structure
+ * @name: Name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
