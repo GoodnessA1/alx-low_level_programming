4689f2432c5627576c1ee7ee6c4dbd24b7c0484e
Goodness <goodnessabisoye@gmail.com>
2023-07-20 13:29:38 -0700

    C ALX

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,23 @@
 #include "variadic_functions.h"
-/**
- * sum_them_all - sums all argument passed together
- * @n: inyeger parameter
- *
- * Return: sum
- */
 
+/**
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum, i;
-	va_list arglist;
+	unsigned int i;
+	int sum = 0;
+	va_list list;
 
-	i = 0;
-	sum = 0;
-	va_start(arglist, n);
-	while (i < n)
-	{
-		sum += va_arg(arglist, int);
-		i++;
-	}
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
 
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,19 +1,28 @@
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int a, i;
-	va_list arglist;
+	unsigned int i;
+	va_list list;
 
-	i = 0;
-	va_start(arglist, n);
-	while (i < n)
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
 	{
-		a = va_arg(arglist, int);
-		printf("%i", a);
-		if (i != (n - 1))
-			printf("%s", separator);
-		i++;
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
 	}
-	_putchar('\n');
+
+	va_end(list);
+
+	printf("\n");
 }
